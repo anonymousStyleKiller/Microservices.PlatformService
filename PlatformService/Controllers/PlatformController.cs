52dc45bc79b248a932d34ab2739bdbfd75e59fe6
Anton <gumb1t97@gmail.com>
2022-06-26 23:10:24 +0300

    feature(PlatformController): Get All platforms

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -1,6 +1,26 @@
-﻿namespace PlatformService.Controllers;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.DTOs;
+using PlatformService.Interfaces;
 
-public class PlatformController
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformController : ControllerBase
 {
-    
+    private readonly IPlatformRepository _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformController(IPlatformRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(_repository.GetAllPlatforms()));
+    }
 }
