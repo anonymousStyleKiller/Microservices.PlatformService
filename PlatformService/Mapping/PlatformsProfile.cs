8514676422f4603b1a472d335b5c8a2867da80c0
Anton <gumb1t97@gmail.com>
2022-06-26 22:26:15 +0300

    feature(Server): Platform Mapping

---
+++ PlatformService/Mapping/PlatformsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Mapping;
+
+public class PlatformsProfile : Profile
+{
+    public PlatformsProfile()
+    {
+        CreateMap<Platform, PlatformReadDto>();
+        CreateMap<PlatformCreateDto, Platform>();
+    }
+}
